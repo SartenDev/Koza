49d679d8339df56d43ac31bc7448499dbcc730d6
Mateusz Kowalczyk <122347770+SartenDev@users.noreply.github.com>
2024-05-16 10:03:27 +0000

    nigga

--- NODEJS/SERWER_Z_BAZA/mojeSQL.js
+++ NODEJS/SERWER_Z_BAZA/mojeSQL.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 
+
 var connection = mysql.createConnection({
     host: "localhost",
     user: "mariadb",
@@ -7,15 +8,13 @@ var connection = mysql.createConnection({
     database: "mariadb"
 })
 
-//console.log(connection)
-connection.query("INSERT INTO users VALUES('Michal','Chebes');", function(err,rows){
-    if(console.log(err)){
-        console.log(err)
-    }
-    else{
-        console.log(rows)
-    }
-})
+
+var sql = "TRUNCATE TABLE users;"
+connection.query(sql)
+
+var sql = "INSERT INTO users VALUES('Szymon','Kot');"
+connection.query(sql)
+    
 connection.query("SELECT * FROM users", function(err,rows){
     if(console.log(err)){
         console.log(err)
@@ -24,3 +23,11 @@ connection.query("SELECT * FROM users", function(err,rows){
         console.log(rows)
     }
 })
+
+// connection.end()
+
+
+
+
+// var sql = "CREATE TABLE szefy IF NOT EXISTS (id int AUTO_INCREMENT PRIMARY KEY, imie VARCHAR(40), nazwisko VARCHAR(40))"
+// connection.query(sql)
